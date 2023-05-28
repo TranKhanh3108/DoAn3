940ac949ac9feba6ef105b032f992d10b6d8d23b
TranKhanh3108 <111827510+TranKhanh3108@users.noreply.github.com>
2023-05-28 14:39:21 +0700

    20. mk

--- customer-password-update.php
+++ customer-password-update.php
@@ -1,12 +1,11 @@
 <?php require_once('header.php'); ?>
 
 <?php
-// Check if the customer is logged in or not
+
 if(!isset($_SESSION['customer'])) {
     header('location: '.BASE_URL.'logout.php');
     exit;
 } else {
-    // If customer is logged in, but admin make him inactive, then force logout this user.
     $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_id=? AND cust_status=?");
     $statement->execute(array($_SESSION['customer']['cust_id'],0));
     $total = $statement->rowCount();
@@ -75,11 +74,11 @@ if (isset($_POST['form1'])) {
                                 }
                                 ?>
                                 <div class="form-group">
-                                    <label for=""><?php echo LANG_VALUE_100; ?> *</label>
+                                    <label for=""><?php echo 'Nhập mật khẩu mới' ?> *</label>
                                     <input type="password" class="form-control" name="cust_password">
                                 </div>
                                 <div class="form-group">
-                                    <label for=""><?php echo LANG_VALUE_101; ?> *</label>
+                                    <label for=""><?php echo 'Nhập lại mật khẩu mới' ?> *</label>
                                     <input type="password" class="form-control" name="cust_re_password">
                                 </div>
                                 <input type="submit" class="btn btn-primary" value="<?php echo LANG_VALUE_5; ?>" name="form1">
